1a1214ae1b0c9b39307fdf278715a7249017542a
mikeshasaco <mikeshasaco@gmail.com>
2025-06-06 19:52:01 -0500

    added new image

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -263,7 +263,7 @@
 .cardone.redmask {
   --color1: rgb(255, 148, 54);
   --color2: rgb(255, 90, 144);
-  background-image: url("gar.png");
+  background-image: url("turquoise.png");
 
 }
 .cardone.qmark {
@@ -369,7 +369,7 @@
 /* https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjU0cjhpeXl3dzBld2xxZ2M1OTN3djB4cmhxOTVzajQwa21lcnk3MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/dAyD6PWoQRs40/giphy.gif */
 .cardone.redmask:after {
   opacity: 1;
-  background-image: url("https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcTBkOGNnYmVjZ3M3Mnl4ZXI0Y3FrbjNlMjdtbDdocW05ejAxYjI1eSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LGv9wanaPoI8Sz7AsT/giphy.gif");
+  background-image: url("https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExejU1bTZqbmJmNjIydnNuMmhtNWhsNnlkMDZsaHFrZHRkNWRiMXUwdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/oNTQZNB67kMf5VHiCj/giphy.gif");
   background-position: center;
   background-size: 250%;
   z-index: 2;
